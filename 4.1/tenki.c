63ec3a6c6d2af5556832a1b585a082135581600b
kaitoyokohama <s1A121700885@toyo.jp>
2021-05-09 17:44:31 +0900

    move folder

---
+++ 4.1/tenki.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    bool tenki = true;
+    if(tenki == true){
+        printf("選択をします\n");
+        printf("散歩に行きます\n");
+    }else{
+        printf("映画を見ます\n");
+    }
+}
