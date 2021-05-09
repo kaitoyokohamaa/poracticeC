befc4492e9fe469da62de8f68f7c9e24b671f86c
kaitoyokohama <s1A121700885@toyo.jp>
2021-05-09 17:43:02 +0900

    4章練習問題

---
+++ 4.1/practice/eat.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    printf("いただきます。\n");
+    printf("バナナを食べます。\n");
+    bool mroe =true;
+    if(mroe){
+        printf("おかわりください\n");
+    }else{
+        printf("お腹がいっぱいです\n");
+    }
+    printf("ごちそうさまでした。\n");
+}
---
+++ 4.1/practice/temp.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+typedef char String[1024];
+
+int main(void)
+{
+    String isHotorCold;
+    int temp =30;
+    printf("暑いですか？ Yes=1 No=0 \n");
+    scanf("%s",isHotorCold);
+    int n =atoi(isHotorCold);
+    while(n == 1){
+        temp=temp-1;
+        printf("現在の設定温度:%d \n",temp);
+        printf("暑いですか？ Yes=1 No=0 \n");
+        scanf("%s",isHotorCold);
+        int n =atoi(isHotorCold);
+        if (n == 0)
+        {
+         n = 0;
+         printf("設定を終了します。\n");
+         break;
+        }
+    }
+}
+
+// answer
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// typedef char String[1024];
+
+// int main(void)
+// {
+//     int temp= 30;
+//     int ansNo = 1;
+//     String answer;
+//     while(ansNo == 1){
+//         printf("現在の設定温度: %d\n",temp);
+//         printf("暑いですか？ Yes=1 No=0 \n");
+//         scanf("%s",answer);
+//         ansNo = atoi(answer);
+//         if(ansNo == 1){
+//             temp = temp - 1;
+//         }else{
+//             printf("設定を終了します\n");
+//         }
+//     }
+//     return 0;
+// }
