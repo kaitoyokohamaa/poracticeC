d19ea04b48d935f98ea0c93d5264cdec1ac9d6af
kaitoyokohama <s1A121700885@toyo.jp>
2021-05-20 09:39:28 +0900

    get adrees

--- 8.1/dutch-treat.c
+++ 8.1/dutch-treat.c
@@ -1,51 +1,95 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+
+
+// typedef char String[1024];
+
+// void showPayment(pay , people , payorg)
+// {
+//     printf("*** 支払額 ***\n");
+//     printf("一人あたり%d円(%d人)、幹事は%d円です。\n",pay,people-1,payorg);
+// }
+
+
+// int calcPayment(amount,people)
+// {
+//     int pay;
+//     int payorg;
+//     double dum;
+//     dum = (double)amount / people;
+//     pay = (int)(dum/100) * 100;
+//     if(dum > pay){
+//         pay = pay +100;
+//     }
+//    payorg = amount - pay * (people - 1);
+//    showPayment(pay,people,payorg);
+//    return 0;
+
+// }
+
+// int iscanf()
+// {
+//     int amount;
+//     int people;
+//     String inputStr;
+//     printf("支出総額を入力してください:");
+//     scanf("%s",inputStr);
+//     amount = atoi(inputStr);
+//     printf("参加人数を入力してください:");
+//     scanf("%s",inputStr);
+//     people = atoi(inputStr);
+//     calcPayment(amount,people);
+//     return 0;
+// }
+
+// int main(void)
+// {
+
+//     iscanf();
+//     return 0;
+// }
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
-
 typedef char String[1024];
 
-void showPayment(pay , people , payorg)
+int iscanf(void)
 {
-    printf("*** 支払額 ***\n");
-    printf("一人あたり%d円(%d人)、幹事は%d円です。\n",pay,people-1,payorg);
-}
-
-
-int calcPayment(amount,people)
-{
-    int pay;
-    int payorg;
-    double dum;
-    dum = (double)amount / people;
-    pay = (int)(dum/100) * 100;
-    if(dum > pay){
-        pay = pay +100;
-    }
-   payorg = amount - pay * (people - 1);
-   showPayment(pay,people,payorg);
-   return 0;
-
-}
-
-int iscanf()
-{
-    int amount;
-    int people;
     String inputStr;
-    printf("支出総額を入力してください:");
     scanf("%s",inputStr);
-    amount = atoi(inputStr);
-    printf("参加人数を入力してください:");
-    scanf("%s",inputStr);
-    people = atoi(inputStr);
-    calcPayment(amount,people);
-    return 0;
+    return atoi(inputStr);
+}
+
+int calcPayment(int dividend,int divisor)
+{
+    double dnum = (double)dividend / divisor;
+
+    int person = (int)(dnum / 100) *100;
+
+    if(dnum > person){
+        person = person + 100;
+    }
+    return person;
+}
+
+void showPayment(int general, int manager, int numbers)
+{
+    printf("***支払額***\n");
+    printf("一人当たり%d円(%d人)、幹事は%d円です。\n",general,numbers - 1,manager);
 }
 
 int main(void)
 {
+    printf("支払総額を入力してください:");
+    int amount = iscanf();
+    printf("参加人数を入力してください:");
+    int people = iscanf();
 
-    iscanf();
+    int pay = calcPayment(amount,people);
+
+    int payorg = amount - pay * (people - 1);
+    showPayment(pay, payorg, people);
     return 0;
 }
-
